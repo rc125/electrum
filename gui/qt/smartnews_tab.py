c321629808779635aa648cfd19479b3b829c97e4
rc125 <rc125@pm.me>
2019-04-23 07:46:19 -0700

    [news] open links e a new window

--- gui/qt/smartnews_tab.py
+++ gui/qt/smartnews_tab.py
@@ -2,9 +2,10 @@ import os
 import traceback
 
 from PyQt5.QtGui import *
-from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWebEngineWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineCore import QWebEngineUrlRequestInterceptor
 from . import util
 
 class SmartnewsTab(QWidget):
@@ -27,14 +28,30 @@ class SmartnewsTab(QWidget):
         self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
 
-        self.webView = QWebEngineView(self)
-        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.webView.sizePolicy().hasHeightForWidth())
-        self.webView.setSizePolicy(sizePolicy)
-        self.webView.setUrl(QUrl("https://electrum-news.rc125.cc/"))
-        self.webView.setObjectName("webView")
+        self.url = QUrl("https://electrum-news.rc125.cc/")
+        self.windows = []
+        self.webView = HtmlView(self.windows)
+        self.webView.load(self.url)
 
         self.verticalLayout.addWidget(self.webView)
         self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
+
+class WebEnginePage(QWebEnginePage):
+    def acceptNavigationRequest(self, url,  _type, isMainFrame):
+        if _type == QWebEnginePage.NavigationTypeLinkClicked:
+            return True
+        return super(WebEnginePage, self).acceptNavigationRequest(url, _type, isMainFrame)
+
+class HtmlView(QWebEngineView):
+    def __init__(self, windows, *args, **kwargs):
+        super(HtmlView, self).__init__(*args, **kwargs)
+        self.setPage(WebEnginePage(self))
+        self._windows = windows
+        self._windows.append(self)
+
+    def createWindow(self, _type):
+        if QWebEnginePage.WebBrowserTab:
+            v = HtmlView(self._windows)
+            v.resize(640, 480)
+            v.show()
+            return v
