b57123085792f9d64ae8e16a052a3df59e31619f
rc125 <rc125@pm.me>
2019-04-24 06:38:32 -0700

    [news] Fix open news links on new window

--- gui/qt/smartnews_tab.py
+++ gui/qt/smartnews_tab.py
@@ -29,29 +29,21 @@ class SmartnewsTab(QWidget):
         self.verticalLayout.setObjectName("verticalLayout")
 
         self.url = QUrl("https://electrum-news.rc125.cc/")
-        self.windows = []
-        self.webView = HtmlView(self.windows)
+        self.webView = QWebEngineView()
+        self.page = WebEnginePage(self.webView)
+        self.webView.setPage(self.page)
         self.webView.load(self.url)
 
         self.verticalLayout.addWidget(self.webView)
         self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
 
 class WebEnginePage(QWebEnginePage):
-    def acceptNavigationRequest(self, url,  _type, isMainFrame):
-        if _type == QWebEnginePage.NavigationTypeLinkClicked:
-            return True
-        return super(WebEnginePage, self).acceptNavigationRequest(url, _type, isMainFrame)
-
-class HtmlView(QWebEngineView):
-    def __init__(self, windows, *args, **kwargs):
-        super(HtmlView, self).__init__(*args, **kwargs)
-        self.setPage(WebEnginePage(self))
-        self._windows = windows
-        self._windows.append(self)
-
     def createWindow(self, _type):
-        if QWebEnginePage.WebBrowserTab:
-            v = HtmlView(self._windows)
-            v.resize(640, 480)
-            v.show()
-            return v
+        page = WebEnginePage(self)
+        page.urlChanged.connect(self.open_browser)
+        return page
+
+    def open_browser(self, url):
+        page = self.sender()
+        QDesktopServices.openUrl(url)
+        page.deleteLater()
