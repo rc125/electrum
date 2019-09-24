68319b2cc0c4714fae376b60c5aaefd7a9208436
rc125 <rc125@pm.me>
2019-09-24 05:55:00 -0700

    [trezor] update python version to build wine

--- contrib/build-wine/prepare-wine.sh
+++ contrib/build-wine/prepare-wine.sh
@@ -13,7 +13,7 @@ LIBUSB_FILENAME=libusb-1.0.22.7z
 LIBUSB_URL=https://prdownloads.sourceforge.net/project/libusb/libusb-1.0/libusb-1.0.22/$LIBUSB_FILENAME?download
 LIBUSB_SHA256=671f1a420757b4480e7fadc8313d6fb3cbb75ca00934c417c1efa6e77fb8779b
 
-PYTHON_VERSION=3.5.4
+PYTHON_VERSION=3.6.8
 
 ## These settings probably don't need change
 export WINEPREFIX=/opt/wine64
@@ -23,8 +23,27 @@ PYHOME=c:/python$PYTHON_VERSION
 PYTHON="wine $PYHOME/python.exe -OO -B"
 
 
+# Set a fixed umask as this leaks into docker containers
+umask 0022
+
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+function info {
+    printf "\r💬 ${BLUE}INFO:${NC}  ${1}\n"
+}
+function fail {
+    printf "\r🗯 ${RED}ERROR:${NC} ${1}\n"
+    exit 1
+}
+function warn {
+    printf "\r⚠️  ${YELLOW}WARNING:${NC}  ${1}\n"
+}
+
+
 # based on https://superuser.com/questions/497940/script-to-verify-a-signature-with-gpg
-verify_signature() {
+function verify_signature() {
     local file=$1 keyring=$2 out=
     if out=$(gpg --no-default-keyring --keyring "$keyring" --status-fd 1 --verify "$file" 2>/dev/null) &&
        echo "$out" | grep -qs "^\[GNUPG:\] VALIDSIG "; then
@@ -35,7 +54,7 @@ verify_signature() {
     fi
 }
 
-verify_hash() {
+function verify_hash() {
     local file=$1 expected_hash=$2
     actual_hash=$(sha256sum $file | awk '{print $1}')
     if [ "$actual_hash" == "$expected_hash" ]; then
@@ -47,15 +66,15 @@ verify_hash() {
     fi
 }
 
-download_if_not_exist() {
+function download_if_not_exist() {
     local file_name=$1 url=$2
     if [ ! -e $file_name ] ; then
-        wget -O $PWD/$file_name "$url"
+        wget -O $file_name "$url"
     fi
 }
 
 # https://github.com/travis-ci/travis-build/blob/master/lib/travis/build/templates/header.sh
-retry() {
+function retry() {
   local result=0
   local count=1
   while [ $count -le 3 ]; do
@@ -88,33 +107,29 @@ wine 'wineboot'
 
 cd /tmp/electrum-smart-build
 
-# Install Python
+echo "Installing Python."
 # note: you might need "sudo apt-get install dirmngr" for the following
 # keys from https://www.python.org/downloads/#pubkeys
-KEYLIST_PYTHON_DEV="531F072D39700991925FED0C0EDDC5F26A45C816 26DEA9D4613391EF3E25C9FF0A5B101836580288 CBC547978A3964D14B9AB36A6AF053F07D9DC8D2 C01E1CAD5EA2C4F0B8E3571504C367C218ADD4FF 12EF3DC38047DA382D18A5B999CDEA9DA4135B38 8417157EDBE73D9EAC1E539B126EB563A74B06BF DBBF2EEBF925FAADCF1F3FFFD9866941EA5BBD71 2BA0DB82515BBB9EFFAC71C5C9BE28DEE6DF025C 0D96DF4D4110E5C43FBFB17F2D347EA6AA65421D C9B104B3DD3AA72D7CCB1066FB9921286F5E1540 97FC712E4C024BBEA48A61ED3A5CA953F73C700D 7ED10B6531D7C8E1BC296021FC624643487034E5"
 KEYRING_PYTHON_DEV="keyring-electrum-build-python-dev.gpg"
-for server in $(shuf -e ha.pool.sks-keyservers.net \
-                        hkp://p80.pool.sks-keyservers.net:80 \
-                        keyserver.ubuntu.com \
-                        hkp://keyserver.ubuntu.com:80) ; do
-    retry gpg --no-default-keyring --keyring $KEYRING_PYTHON_DEV --keyserver "$server" --recv-keys $KEYLIST_PYTHON_DEV \
-    && break || : ;
-done
+gpg --no-default-keyring --keyring $KEYRING_PYTHON_DEV --import "$here"/gpg_keys/7ED10B6531D7C8E1BC296021FC624643487034E5.asc
 for msifile in core dev exe lib pip tools; do
     echo "Installing $msifile..."
-    wget -N -c "https://www.python.org/ftp/python/$PYTHON_VERSION/win32/${msifile}.msi"
-    wget -N -c "https://www.python.org/ftp/python/$PYTHON_VERSION/win32/${msifile}.msi.asc"
+    download_if_not_exist "${msifile}.msi" "https://www.python.org/ftp/python/$PYTHON_VERSION/win32/${msifile}.msi"
+    download_if_not_exist "${msifile}.msi.asc" "https://www.python.org/ftp/python/$PYTHON_VERSION/win32/${msifile}.msi.asc"
     verify_signature "${msifile}.msi.asc" $KEYRING_PYTHON_DEV
-    wine msiexec /i "${msifile}.msi" /qb TARGETDIR=C:/python$PYTHON_VERSION
+    wine msiexec /i "${msifile}.msi" /qb TARGETDIR=$PYHOME
 done
 
 # upgrade pip
+echo "Upgrade pip"
 $PYTHON -m pip install pip --upgrade
 
 # Install pywin32-ctypes (needed by pyinstaller)
+echo "Install pywin32-ctypes (needed by pyinstaller)"
 $PYTHON -m pip install pywin32-ctypes==0.1.2
 
 # install PySocks
+echo "install PySocks"
 $PYTHON -m pip install win_inet_pton==1.0.1
 
 $PYTHON -m pip install -r $here/../deterministic-build/requirements-binaries.txt
